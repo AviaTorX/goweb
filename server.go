7e5be5f1d2f9ccac1b3be982702d13a68577d342
AviaTorX <shinde91avinash@gmail.com>
2019-12-17 04:03:01 +0530

    intial route handler

--- server.go
+++ server.go
@@ -6,7 +6,12 @@ import (
 	"log"
 )
 
+func firstConnectioon(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi Axi")
+}
+
 func CreateServer(port string, router string) {
 	fmt.Printf("Starting server...\n")
+	http.HandleFunc("/", firstConnectioon)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
