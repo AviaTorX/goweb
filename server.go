26045a12fa9fa51c191aa25c9a32ce8ab10a81c8
AviaTorX <shinde91avinash@gmail.com>
2019-12-17 04:25:12 +0530

    additional code- mock values

--- server.go
+++ server.go
@@ -7,11 +7,19 @@ import (
 )
 
 func firstConnectioon(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
 	fmt.Fprintf(w, "Hi Axi")
 }
 
 func CreateServer(port string, router string) {
 	fmt.Printf("Starting server...\n")
 	http.HandleFunc("/", firstConnectioon)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
